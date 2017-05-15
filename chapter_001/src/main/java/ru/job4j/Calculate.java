46574e5d08fbe578d14613175c0e6a52306269c6
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2017-05-15 21:30:15 +0300

    task 8054

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,16 @@
 package ru.job4j;
  
+/**
+ * class Calculate первая программа 
+ * Вывод строки "Hello World" в консоль
+ *
+ * @author Pavel Ivanov (pavel.ivanov.060988@gmail.com)
+ * @version $Id$
+ * @since 15.05.2017
+ */
+ 
 public class Calculate {
+	
 	public static void main(String[] args) {
         System.out.println("Hello World");
     }
